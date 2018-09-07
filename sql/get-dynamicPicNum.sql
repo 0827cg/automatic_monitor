0284f7e0260f2c2e3ca82bfc6904c72e0638722d
cg <1732821152@qq.com>
2018-09-07 19:17:57 +0800

    add more for msg for sql

--- sql/get-dynamicPicNum.sql
+++ sql/get-dynamicPicNum.sql
@@ -3,7 +3,7 @@
     time: 2018-04-24
     description: 获取规定时间内动态项中上传了的图片总数
 */
-
+/*
 SELECT
 	ROUND(sum((LENGTH(files)-LENGTH(replace(files,'http', ''))) / 4),0) AS dynamicPicNum
 FROM
@@ -14,3 +14,14 @@ AND appendtime <= %d
 AND effect = 1
 AND active = 1
 AND tags is null
+*/
+
+SELECT
+	ROUND(sum((LENGTH(files)-LENGTH(replace(files,'http', ''))) / 4),0) AS dynamicPicNum
+FROM
+	htg_comments
+WHERE
+	append_time > %d
+AND append_time <= %d
+AND effect = 1
+AND active = 1
--- sql/get-foodPicNum.sql
+++ sql/get-foodPicNum.sql
@@ -3,7 +3,7 @@
     time: 2018-04-24
     description: 获取规定时间内食谱项中上传了的图片总数
 */
-
+/*
 SELECT
 	ROUND(sum((LENGTH(files)-LENGTH(replace(files,'http', ''))) / 4),0) AS foodPicNum
 FROM
@@ -14,3 +14,14 @@ AND appendtime <= %d
 AND effect = 1
 AND active = 1
 AND tags is not null
+*/
+
+SELECT
+	ROUND(sum((LENGTH(files)-LENGTH(replace(files,'http', ''))) / 4),0) AS foodPicNum
+FROM
+	htg_cookbook_record
+WHERE
+	pre_insert_time > %d
+AND pre_insert_time <= %d
+AND effect = 1
+AND active = 1
